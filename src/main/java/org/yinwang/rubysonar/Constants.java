26395d3fcb479f417cbca220e49292edb44b412f
hc <handsomecheung@gmail.com>
2019-08-01 11:11:38 +0800

    copy all instance and class variables

--- src/main/java/org/yinwang/rubysonar/Constants.java
+++ src/main/java/org/yinwang/rubysonar/Constants.java
@@ -5,4 +5,6 @@ public class Constants {
     public static final String INSTNAME = "#this";
     public static final String IDSEP = "^";
     public static final String IDSEP_REGEX = "\\^";
+    public static final String IVAR_PREFIX = "@";
+    public static final String CVAR_PREFIX = "@@";
 }
